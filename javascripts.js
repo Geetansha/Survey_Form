f844ee87b78fb709b1cd860351fd193ffc0868c7
Geetansha <97462299+Geetansha@users.noreply.github.com>
2023-06-24 22:01:11 +0530

    Add files via upload

---
+++ javascripts.js
@@ -0,0 +1,12 @@
+function submitForm(){
+    if(document.getElementsByClassName("nameInput").value=="" ||
+        document.getElementsByClassName("commentsInput").value=="" ||
+        document.getElementsByClassName("aresInput").value=="" ||
+        document.getElementsByClassName("emailInput").value=="" ||
+        document.getElementsByClassName("roleInput").value==""  ){
+
+    }
+    else{
+        alert("Submitted Successfully Thanks!!");
+    }
+}
